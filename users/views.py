ccaada4c528566a6053d34759cf8d0b1de476f44
Sabalevskiy Sergey <sabal2000@mail.ru>
2019-02-07 01:39:10 +0300

    added registration

---
+++ users/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
+from django.contrib.auth.forms import AuthenticationForm
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('blog-home')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
+
+
+def login(request):
+    form = AuthenticationForm()
+    return render(request, 'users/login.html', {'form': form})
