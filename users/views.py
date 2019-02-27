a8bf3238bfc3e7cea163aff5b21be91fbb3e4bd0
Sabalevskiy Sergey <sabal2000@mail.ru>
2019-02-27 22:28:52 +0300

    added login

--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
+
+from .forms import UserRegisterForm
 
 
 def register(request):
@@ -10,8 +12,8 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'Account created. Log in')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
@@ -20,3 +22,8 @@ def register(request):
 def login(request):
     form = AuthenticationForm()
     return render(request, 'users/login.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
